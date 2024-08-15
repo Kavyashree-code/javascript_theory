b4c0a995db2c56f23278666b6e91fb4fad76d043
Kavya <Kavya@gmail.com>
2024-08-15 18:36:17 +0530

    practice1

---
+++ practice js/script.js
@@ -0,0 +1,39 @@
+// // debugger
+// let userName = 'Kavya shree'
+// let userIntro = 'I am a software developer'
+// let age = 25
+// let isHappy = true
+// //use let for new variable creation not for new box with old things but can use
+// newUserName = userName
+// detailsOfUser = userIntro + 'working at xyz company' +' '+ 'my name is ' +' '+ userName
+// let firstName = 'Radha'
+// let lastName = 'krishna'
+// fullName = 'My fullName is' + ' '+ firstName + lastName
+
+// let a = undefined
+// let b
+// //js put undefined its own, even we don't assign a value
+
+// //same variable name can be changed
+// let myName = 'krutika'
+// myName = 'kavya'
+
+// //another ex
+// let colorLike = 'Black'
+// colorLike = 188
+
+// let a
+// let fullName = 'Hi i am '+a+' here'
+// //con = Hi i am undefined here
+
+// let a
+// a = 'shilpa'
+// fullName = 'hi my name is '+ a
+// //con = hi my name is shilpa
+
+// let a
+// fullName = 'hey my name is '+ a
+// a = 'kavya'
+// //con = hey my name is undefined bez we wrote code then defined
+
+
